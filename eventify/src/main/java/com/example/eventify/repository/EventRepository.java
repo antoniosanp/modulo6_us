afc28b5f16488d2e3352418cf11b654626e8645b
antoniosanp <antonio.pulgarin97@gmail.com>
2026-05-13 08:40:01 -0500

    Refactor: EventRepository

--- eventify/src/main/java/com/example/eventify/repository/EventRepository.java
+++ eventify/src/main/java/com/example/eventify/repository/EventRepository.java
@@ -1,33 +1,12 @@
 package com.example.eventify.repository;
 
 import com.example.eventify.model.Event;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-
 @Repository
-public class EventRepository {
+public interface EventRepository  extends JpaRepository<Long, Repository> {
 
-    private final Map<Integer, Event> eventMap;
+    Event findByName(String name);
 
-    public EventRepository() {
-        this.eventMap = new LinkedHashMap<>();
-    }
-
-    public List<Event> getAllEvents() {
-        return new ArrayList<>(eventMap.values());
-    }
-
-    public Optional<Event> getEventById(int id) {
-        return Optional.ofNullable(eventMap.get(id));
-    }
-
-    public Event addEvent(Event event) {
-        eventMap.put(event.getId(), event);
-        return event;
-    }
 }
