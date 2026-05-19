7d05229fd8f88e2cccc9b7997dbaea407fbb22c6
antoniosanp <antonio.pulgarin97@gmail.com>
2026-05-19 11:56:38 -0500

    View: events

---
+++ eventify/src/main/java/com/example/eventify/controller/ui/EventUIController.java
@@ -0,0 +1,34 @@
+package com.example.eventify.controller.ui;
+
+
+import com.example.eventify.model.Event;
+import com.example.eventify.service.EventService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/eventos/lista")
+public class EventUIController {
+
+    private final EventService eventService;
+
+    @GetMapping
+    public String listarEventosUI(Model model){
+        List<Event> eventos = eventService.getAllEvents();
+
+        model.addAttribute("eventos", eventos);
+        model.addAttribute("tituloPantalla", "lista de eventos");
+
+        return "events/lista";
+
+    }
+
+
+}
