aab58623dc8c8bef3e81bd6c7c124dca7a8708f9
antoniosanp <antonio.pulgarin97@gmail.com>
2026-05-20 08:20:23 -0500

    feature: pagination & create event form

--- eventify/src/main/java/com/example/eventify/controller/ui/EventUIController.java
+++ eventify/src/main/java/com/example/eventify/controller/ui/EventUIController.java
@@ -1,36 +1,51 @@
 package com.example.eventify.controller.ui;
 
 
+import com.example.eventify.dto.EventDTO;
 import com.example.eventify.model.Event;
 import com.example.eventify.service.EventService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/eventos/lista")
+@RequestMapping("/eventos")
 public class EventUIController {
 
     private final EventService eventService;
 
-    @GetMapping
-    public String listarEventosUI(Model model, Pageable pageable){
+    @GetMapping("/list")
+    public String listarEventosUI(Model model, @PageableDefault(size = 20) Pageable pageable){
+
         Page<Event> eventos = eventService.getAllEvents(pageable);
 
         model.addAttribute("eventos", eventos);
         model.addAttribute("tituloPantalla", "lista de eventos");
 
         return "events/lista";
-
     }
 
+    @GetMapping("/nuevo")
+    public String mostrarFormularioEvento(Model model){
+
+        model.addAttribute("evento", new EventDTO());
+        model.addAttribute("tituloPantalla", "Registrar nuevo evento");
+        return "events/formulario";
+    }
+
+    @PostMapping("/guardar")
+    public String guardarEvento(@ModelAttribute("evento") EventDTO event){
+
+        eventService.addEvent(event);
+
+        return "redirect:/eventos/list";
+    }
 
 }
